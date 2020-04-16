dd1d55a9970dfda1fd3bdc552675e34b3e6cb5fe
Brendan Hosking <brendan.hosking@csiro.au>
2020-04-16 15:17:48 +1000

    FEAT: Implement automatic cloudfront invalidation

--- s3.tf
+++ s3.tf
@@ -15,9 +15,26 @@ locals {
 resource aws_s3_bucket website_bucket {
   bucket = "covid19-platform"
 
+  versioning {
+    enabled = true
+  }
+
   website {
     redirect_all_requests_to = "https://${var.domain_name}"
   }
+}
+
+resource aws_s3_bucket_notification refreshCloudfront {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  lambda_function {
+    lambda_function_arn = module.lambda_refreshCloudfront.function_arn
+    events = [
+      "s3:ObjectCreated:*",
+    ]
+  }
+
+  depends_on = [aws_lambda_permission.s3_refreshCloudfront]
 }
 
 resource aws_s3_bucket_object website_index {
