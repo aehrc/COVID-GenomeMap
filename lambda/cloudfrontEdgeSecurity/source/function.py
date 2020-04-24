36fe9f46584755f0d967d393dfe83cd08ff79722
Brendan Hosking <brendan.hosking@csiro.au>
2020-04-24 12:19:18 +1000

    FIX: Allow txt file to be inserted into homepage

--- lambda/cloudfrontEdgeSecurity/source/function.py
+++ lambda/cloudfrontEdgeSecurity/source/function.py
@@ -3,13 +3,13 @@ import json
 STATIC_SECURITY_HEADERS = {
     'Content-Security-Policy': '; '.join([
         "default-src https: 'unsafe-eval' 'unsafe-inline'",
-        "frame-ancestors 'none'",
+        "frame-ancestors 'self'",
         "img-src 'self' data:",
         "object-src 'self'",
     ]),
     'X-XSS-Protection': '1; mode=block',
     'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
-    'X-Frame-Options': 'DENY',
+    'X-Frame-Options': 'SAMEORIGIN',
     'Expect-CT': 'Expect-CT: max-age=604800, enforce',
     'X-Content-Type-Options': 'nosniff',
     'Feature-Policy': "autoplay 'none'; camera 'none'; microphone 'none'",
